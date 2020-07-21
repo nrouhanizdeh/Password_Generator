dedd68273ad9bf99dcc9697364bf790796b1be25
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-07-21 14:36:02 -0500

    added funtion to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -2,61 +2,69 @@
 var generateBtn = document.querySelector("#generate");
 
 // prompt variables
-var minChar = 4;
-var maxChar = 10;
-var availableChar = ["lowercase","numeric","numeric","special"];
-var lowerStr = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-var upperStr = lowerStr.toUpperCase();
-var numeric = "0123456789";
-var specialChar = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-var password = "";
-var character = "";
+var minChar = 8;
+var maxChar = 128;
+var availableChar = ["lowercase","uppercase","numeric","special"];
+// var lowerStr = "abcdefghijklmnopqrstuvwxyz"; //to upper 
+// var upperStr = lowerStr.toUpperCase();
+// var numeric = "0123456789";
+// var specialChar = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+var charSet = {
+  "lowercase": "abcdefghijklmnopqrstuvwxyz",
+  "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  "numeric": "0123456789",
+  "special": "!@#$%^&*()_+~`|}{[]\:;?><,./-="
+};
 
+var password="";
 
-minChar = prompt("Minimum number of characters?");
-if (minChar<=0 || isNaN(minChar) || !minChar) {
-  alert("Invalid response. Default minimum of 4 characters will be used.");
-  minChar = 4;
-}
-maxChar = prompt("Max number of characters?");
-if (maxChar>1000 || maxChar<0 || isNaN(maxChar || !maxChar)) {
-  alert("Invalid response. Default max of 10 characters will be used");
-  minChar = 10;
-}
-var selectType = prompt("Choose one or more character types (comma separated): lowercase, uppercase, numeric, and/or special ");
-selectType = selectType.split(',');
-var typeChar = ["test"];
-for(var item in selectType){
-  item = item.trim();
-  console.log(availableChar);
-  if(availableChar.includes(item)){
-    typeChar = typeChar.push(item);
-  }
-  if( typeChar.length === 0 ){
-    typeChar = availableChar;
-  }
-}
-console.log(typeChar);
-
+// Generate passwork
 function generatePassword() {
 
-  while(password.length<maxChar)
-    entity1 = Math.ceil(lowerStr.length * Math.random());
-    entity2 = Math.ceil(numeric.length * Math.random());
-    entity3 = Math.ceil(specialChar.length * Math.random());
-    entity4 = Math.ceil(upperStr.length * Math.random());
-  hold = lowerStr.charAt( entity1 );
-  hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-  character += hold;
-  character += numeric.charAt( entity2 );
-  character += specialChar.charAt( entity3 );
-  password = character;
+  // prompt the user for the number of characters
+  var numChar = prompt("What would you like the password length to be? it has to be between 8 and 128 characters");
+  if (numChar<minChar || isNaN(minChar) || !numChar || numChar>maxChar) {
+    alert("Invalid response. Default minimum of 8 characters will be used.");
+    numChar = 8;
+  }
 
-  password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-  return password.substr(0,maxChar);
+  // prompt the user for character types
+  var selectType = prompt("Choose one or more character types (comma separated): lowercase, uppercase, numeric, and/or special ");
+  if(selectType){
+    selectType = selectType.split(',');
+  }else{
+    alert("Invalid response. All four character types will be used.");
+    selectType = availableChar;
+  }
+
+  var typeChar = [];
+  for(var i=0 ; i < selectType.length ; i++){
+    if(availableChar.includes(selectType[i].trim())){
+      typeChar.push(selectType[i].trim());
+    }
+  }
+  if(typeChar == null){
+    alert("Invalid response. All four character types will be used.");
+    typeChar = availableChar;
+  }
+
+  for(i=0 ; i<typeChar.length ; i++){
+    password += charSet[typeChar[i]][randomInt(charSet[typeChar[i]].length)]
+  }
+  for(i=0; i<numChar-(typeChar.length); i++){
+    var randomTypeChar = typeChar[randomInt(typeChar.length)];
+    password += charSet[randomTypeChar][randomInt(charSet[randomTypeChar].length)];
+  }
+  return password;
 }
 
-console.log( generatePassword() );
+// Generate a random integer for a given range
+function randomInt(n) {
+	var x = Math.floor(Math.random() * n);
+	if (x < 0 || x >= n)
+		throw "Arithmetic exception";
+	return x;
+}
 
 // Write password to the #password input
 function writePassword() {
